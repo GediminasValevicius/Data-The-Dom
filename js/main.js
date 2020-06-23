d05d8965da01d530c7af1e577fbb86290cd63b21
GediminasValevicius <gediminas.valevicius@outlook.com>
2020-06-23 22:17:16 +0100

    removed commas at the top of the table

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ function writeToDocument(url) {
             });
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`.replace(/,/g, "");
         // document.getElementById("data").innerHTML = results;
     });
 }
